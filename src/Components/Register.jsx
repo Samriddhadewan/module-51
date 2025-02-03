a1ee5a254ff08bddd64f847f362941d63bda5ede
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-02-03 15:16:39 +0600

    video 2 done

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,7 +1,48 @@
+import { Link } from "react-router-dom"
 
 const Register = () => {
+  const handleRegister = e => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+
+    console.log(name, email, password)
+  }
   return (
-    <div>Register</div>
+    <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <h1 className="text-4xl text-center p-8 font-bold">Register Now</h1>
+      <form onSubmit={handleRegister} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="email" name="name" placeholder="Enter name" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input name="password" type="password" placeholder="password" className="input input-bordered" required />
+          
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Register</button>
+        </div>
+        <p>All ready have an account ? <Link className="underline text-blue-500" to="/register">Register now</Link></p>
+      </form>
+    </div>
+  </div>
+</div>
   )
 }
 
